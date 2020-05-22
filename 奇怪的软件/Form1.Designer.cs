42a5e7f51b17d660129ed01106b27adc6bae8d9b
Kagiri <1518412054@qq.com>
2020-05-22 23:35:13 +0800

    what
    This is the project of visual studio 2019.

---
+++ 奇怪的软件/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace nene1212
+{
+    partial class zuzu
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(zuzu));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "帅";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "不帅";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            // 
+            // button3
+            // 
+            this.button3.CausesValidation = false;
+            this.button3.Cursor = System.Windows.Forms.Cursors.No;
+            this.button3.Location = new System.Drawing.Point(163, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 28);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "up主帅吗";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 274);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 30);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "up长得真丑";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(395, 274);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 30);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "点到有惊喜";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.MouseEnter += new System.EventHandler(this.button5_MouseEnter);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "nene1212";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // zuzu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 316);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "zuzu";
+            this.Text = "nene1212";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 奇怪的软件/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nene1212
+{
+    public partial class zuzu : Form
+    {
+        public zuzu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.Text = "帅";
+            button2.Text = "不帅";
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            button2.Text = "帅";
+            button1.Text = "不帅";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你怎么知道！");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你都把我说不好意思了");
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            button4.Text = "up帅炸了";
+            MessageBox.Show("被你发现了ww");
+        }
+
+        private void button5_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int x = random.Next(0, base.Size.Width - this.button5.Size.Width);
+            int y = random.Next(0, base.ClientSize.Height - this.button5.Size.Height);
+            Point location = new Point(x, y);
+            this.button5.Location = location;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你也很帅嘛！");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("up已经帅出天际了吧？", "人生的抉择", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                MessageBox.Show("=w=");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("这是up的id哦，赶快bilibili关注一波ww");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("我在问你诶"); 
+        }
+    }
+}
